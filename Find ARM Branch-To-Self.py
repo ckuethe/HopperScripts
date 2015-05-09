b2cd6ea85ffecc07a0bbc5fc17da5686bb0377b6
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-05-09 12:53:22 -0700

    a better way to detect branch-to-self

--- Find ARM Branch-To-Self.py
+++ Find ARM Branch-To-Self.py
@@ -2,39 +2,42 @@
 # used to wait for an event such as an interrupt.
 # Based off of the "Create Procedures ARM" script by bradenthomas@me.com
 
-import struct,sys
+import time
 
-# configuration parameters, adjust as needed:
-ENDIANNESS = "<" # Little endian = <, Big endian = >
-
-# helper methods
-def read_data(segment, addr, dlen):
-    return "".join([chr(segment.readByte(addr+x)) for x in range(0,dlen)])
-
-# First, we disassemble the whole segment
+# main loop
 doc = Document.getCurrentDocument()
-seg = doc.getCurrentSegment()
-if not seg:
-    raise Exception("No segment selected")
-seg.disassembleWholeSegment()
+start_time = time.clock()
+nsegs = doc.getSegmentCount()
+n = 0
 
-# Get segment starting address
-addr = seg.getStartingAddress()
-last = addr + seg.getLength()
-while addr < last:
-    # Find the next unexplored area
-    addr=seg.getNextAddressWithType(addr,Segment.TYPE_CODE)
-    if addr == Segment.BAD_ADDRESS:
-        break
+for seg_id in range(0, nsegs):
+	doc.log("scanning segment %d/%d" % (seg_id+1, nsegs))
+	seg = doc.getSegment(seg_id)
 
-    # Copy a 16-bit value to see if it is in thumb mode
-    try:
-        halfword_value, = struct.unpack(ENDIANNESS+"H", read_data(seg, addr, 2))
-        if halfword_value & 0xffff == 0xe7fe: # Branch to self
-            seg.setNameAtAddress(addr, "loc_%x_bts" % addr)
-    except:
-        continue
+	seg_start = seg.getStartingAddress()
+	seg_len = seg.getLength()
+	seg_stop = seg_start + seg_len
 
-    addr += 2
+	# thumb instructions are 2-byte aligned
+	if seg_start % 2 == 1:
+		seg_start += 1
+		seg_len -= 1
+
+	for addr in range(seg_start, seg_stop):
+		i = addr - seg_start
+		if (i % 10000 == 0):
+			doc.log("%.1f%% " % (i * 100.0 / seg_len) )
+
+		x = seg.readByte(addr) & 0xff
+		y = seg.readByte(addr+1) & 0xff
+		if ((x == 0xe7) and (y ==0xfe)): # Branch to self
+			seg.setNameAtAddress(addr, "loc_%x_bts" % addr)
+			seg.markAsCode(addr)
+			doc.log("loc_%x_bts" % addr)
+			n += 1
+
+		addr += 2
 
 doc.refreshView()
+elapsed = (time.clock() - start_time)
+doc.log("found %d branch-to-self in %0.1f seconds" % (n, elapsed))
